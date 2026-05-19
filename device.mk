da3da69f1740bc66f6b2bf05ec42731b577b1828
hiroshi. <hiroshi@tanuki.page>
2026-05-19 00:22:24 -0400

    FroggerPro: Set basic audio config

--- device.mk
+++ device.mk
@@ -13,3 +13,11 @@ TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_VARIANT := kryo300
 TARGET_CPU_VARIANT_RUNTIME := oryon
+
+# Audio
+$(call soong_config_set, android_hardware_audio, run_64bit, true)
+PRODUCT_PACKAGES += \
+    android.hardware.audio.service
+
+AUDIO_HAL_DIR := hardware/qcom-caf/sm8750/audio/primary-hal
+AUDIO_PAL_DIR := hardware/qcom-caf/sm8750/audio/pal
