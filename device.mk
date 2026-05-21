1defc052765e46a17a9adf6c6c5e26b77a86b66f
LuK1337 <priv.luk@gmail.com>
2026-05-20 23:07:12 -0400

    FroggerPro: Enable QTI recovery minui backend

--- device.mk
+++ device.mk
@@ -48,6 +48,9 @@ TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+# Recovery 
+$(call soong_config_set_bool,recovery,target_recovery_uses_qti_drm,true)
+
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
