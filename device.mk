f63783ab90d9e3484e12f72f5d529af714b5b016
hiroshi. <hiroshi@tanuki.page>
2026-05-22 02:46:36 -0400

    FroggerPro: Import config.fs from LA.VENDOR.15.4.0.r1-19500-pakala.QSSI16.0

--- device.mk
+++ device.mk
@@ -86,6 +86,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# Filesystem
+TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
+
 
 # Graphics
 PRODUCT_COPY_FILES += \
