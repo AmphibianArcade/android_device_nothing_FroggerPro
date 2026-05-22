6cf6e71f5ffb88fabe0bd257b40541475c25dac9
kdrag0n <danny@kdrag0n.dev>
2026-05-22 02:30:57 -0400

    FroggerPro: Switch to Vulkan UI renderer

--- device.mk
+++ device.mk
@@ -97,6 +97,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2024-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2024-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+
+# HWUI
+TARGET_USES_VULKAN := true
+
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 
