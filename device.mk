8122ec1c73d621c4a7b440fe5183c718bb267367
hiroshi. <hiroshi@tanuki.page>
2026-06-01 20:08:09 -0400

    FroggerPro: Build memtrack service from source

--- device.mk
+++ device.mk
@@ -158,6 +158,10 @@ PRODUCT_PACKAGES += \
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 
+# Memtrack
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.memtrack-service
+
 # Overlays
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rro_overlays/partition_order.xml:$(TARGET_COPY_OUT_PRODUCT)/overlay/partition_order.xml
