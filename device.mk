dc6162b7c6215d6a4bc0c85809cecc9732d61c58
hiroshi. <hiroshi@tanuki.page>
2026-06-16 03:55:40 -0400

    FroggerPro: Copy missing usb configs

--- device.mk
+++ device.mk
@@ -226,7 +226,12 @@ PRODUCT_PACKAGES += \
     android.hardware.usb-service.qti \
     android.hardware.usb.gadget-service.qti \
     init.qcom.usb.rc \
-    init.qcom.usb.sh
+    init.qcom.usb.sh \
+    usb_compositions.conf
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # WiFi
 PRODUCT_PACKAGES += \
