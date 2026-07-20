a59fc60c90666f8d6b39bf26cff7c610622affcb
hiroshi. <hiroshi@tanuki.page>
2026-07-20 01:40:08 -0400

    FroggerPro: Build NothingFelicaDisabler
    To disable FeliCa apps on non JPN SKUs

--- device.mk
+++ device.mk
@@ -183,6 +183,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# FeliCa
+PRODUCT_PACKAGES += \
+    NothingFelicaDisabler
+
 # GPS
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
