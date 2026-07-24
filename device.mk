3ea86f5c5bd2096d0a20a104ef341bf906bc7bd1
hiroshi. <hiroshi@tanuki.page>
2026-07-23 21:59:13 -0400

    FroggerPro: Switch to OSS fingerprint service

--- device.mk
+++ device.mk
@@ -115,6 +115,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
 # Biometrics
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint-service.nothing
+
+$(call soong_config_set_bool,nothing_fingerprint,use_lhbm,true)
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
